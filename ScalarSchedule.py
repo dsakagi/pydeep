9a89f26c00522b6d036d5460d0645c3e8782d3fa
Dallin Akagi <dallin@datarobot.com>
2013-10-02 00:51:45 -0400

    Starting to add some docstring

--- ScalarSchedule.py
+++ ScalarSchedule.py
@@ -1,7 +1,25 @@
+'''This module contains a few classes for determining learning schedules.
+There are several hyperparameters used during training of a deep learning
+beast, and different methods are employed for tweaking them over the
+various training epochs
+'''
+
 import numpy as np
 
 
+
+
 class ConstantSchedule:
+    '''The scalar never changes - it always returns the value
+    with which it was created
+
+    Parameters
+    ----------
+    v : float
+        The scalar to return
+
+    '''
+
     def __init__(self, v):
         self.v = v
 
@@ -9,13 +27,31 @@ class ConstantSchedule:
         return self.v
 
 class LinearSchedule:
+    '''Encodes a schedule which changes with a linear update.  That is,
+    the same incremental step is applied to the scalar each time the
+    ``get`` function is called.
+
+    Parameters
+    ----------
+    init : float
+        The initial value of the scalar
+
+    final : float
+        The final value of the scalar. Once this value is reached, it is
+        always returned
+
+    n_steps : int
+        Over this many epochs the value of this scalar will increase linearly
+        beginning at ``init`` and ending at ``final``
+
+    '''
     def __init__(self, init, final, n_steps):
         self.v = init
         self.final = final
         self.max_steps = n_steps
         self.step = 0
         self.delta = (final - init) / n_steps
-    
+
     def get(self):
         val = self.v
         if self.step < self.max_steps:
@@ -24,6 +60,19 @@ class LinearSchedule:
         return val
 
 class ExponentialSchedule:
+    '''A scalar which grows/shrinks exponentially.  There is no upper/lower
+    which can be set, so beware of numerical issues.
+
+    Parameters
+    ----------
+    init : float
+        The initial value of the parameter
+
+    decay : float
+        The decay (or growth) multilpier which is applied to the scalar at
+        each epoch
+
+    '''
     def __init__(self, init, decay):
         self.v = init
         self.decay = decay
--- views.py
+++ views.py
@@ -1,16 +1,25 @@
 import numpy as np
 
 def isSquare(x):
+    '''A boolean question.  I should probably test
+    if x is an int, but I don't
+    '''
     return int(np.sqrt(x))**2 == x
 
-def SquareDataGetView(data, nSamples=100):
+def square_data_get_view(data, nSamples=100):
+    '''If you have squashed your image data to be row vectors, then
+    this will undo that.  For now it assumes that you are only operating
+    on square images (cough, cough, MNIST, cough).  The view that is
+    returned can be visualized directly with matplotlib
+
+    '''
     if not isSquare(data.shape[1]):
         print 'Need square data, given ' + str(data.shape[0])
         return np.zeros(nSamples * data.shape[0])
 
     sidelen = int(np.sqrt(data.shape[0]))
     nToView = nSamples if data.shape[1] > nSamples else data.shape[1]
-    
+
     viewRows = int(np.sqrt(nToView))
     viewCols = nToView / viewRows if 0 == nToView % viewRows else int(np.floor(ntoView / viewRows) + 1)
 
@@ -22,6 +31,6 @@ def SquareDataGetView(data, nSamples=100):
             sample = data[:,sampleIdx].reshape(sidelen, sidelen)
             view[row*sidelen:(row+1)*sidelen,col*sidelen:(col+1)*sidelen] = sample
             sampleIdx += 1
-    
+
     return view
-            
+
