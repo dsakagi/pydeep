be0193662642fc3e2330faed8e2298d120ee20e8
Dallin Akagi <dallin@datarobot.com>
2013-10-11 01:11:03 -0400

    netTest will run.  not so good, though

--- nettest/netTest.py
+++ nettest/netTest.py
@@ -1,17 +1,16 @@
 import os
 import sys
-sys.path.append('..')
 import numpy as np
-import Learning_rm as Learning
+import pydeep
+import pydeep.neural
 
 import pylab
-import utils
-import Preprocess
+import pydeep.utils
+import pydeep.utils.utils
 
 
-
-mnist_dir = os.path.join(os.environ['DATA_HOME'], 'mnist')
-mnist_train_path = os.path.join(mnist_dir, 'MNISTTrainData.npy')
+mnist_dir = os.path.join(os.environ['PYDEEP_HOME'], 'demo', 'mnist')
+mnist_train_path = os.path.join(mnist_dir, 'train-images-idx3-ubyte.npy')
 
 data = np.load(mnist_train_path)
 
@@ -24,37 +23,37 @@ ViewDimensions = (10, 10)   # Should multiply to nHidden
 
 arch = [train.shape[1], nHidden, train.shape[1]]
 lts = ('Logistic', 'Logistic')
-reg = Learning.NetReg()
+reg = pydeep.neural.NetReg()
 reg.dropout = True
 reg.max_constraint = True
 regs = [reg] * 2
 regs[0].drop_rate = 0.2
 regs[1].drop_rate = 0.5
 
-net = Learning.Net(arch, lts, regs)
+net = pydeep.neural.Net(arch, lts, regs)
 
-ntp = Learning.NetTrainParams()
+ntp = pydeep.neural.NetTrainParams()
 ntp.maxepoch = 300
-ntp.eta = Learning.ExponentialSchedule(1.0, 0.98)
+ntp.eta = pydeep.scalar_schedule.ExponentialSchedule(1.0, 0.98)
 
-Learning.train_sgd(net, train, train, ntp)
+pydeep.neural.train_sgd(net, train, train, ntp)
 
 
 
-Wimg = utils.tile_raster_images(net.Layers[0].W.T, (28, 28), ViewDimensions)
+Wimg = pydeep.utils.utils.tile_raster_images(net.Layers[0].W.T, (28, 28), ViewDimensions)
 
 pylab.figure(1)
 pylab.imshow(Wimg)
 pylab.set_cmap('gray')
 pylab.axis('off')
 
-dimg_src = utils.tile_raster_images(train, (28,28), ViewDimensions)
+dimg_src = pydeep.utils.utils.tile_raster_images(train, (28,28), ViewDimensions)
 pylab.figure(2)
 pylab.imshow(dimg_src)
 pylab.axis('off')
 
 X = net.predict(train)
-dimg_rec = utils.tile_raster_images(X, (28,28), ViewDimensions)
+dimg_rec = pydeep.utils.utils.tile_raster_images(X, (28,28), ViewDimensions)
 pylab.figure(3)
 pylab.imshow(dimg_rec)
 pylab.axis('off')
