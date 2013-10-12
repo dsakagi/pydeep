77833efc6fef67e0265fc006fbf511d34a746946
Dallin Akagi <dallin@datarobot.com>
2013-10-11 21:12:08 -0400

    Timing test now running

--- debug/rbmTimingTest.py
+++ debug/rbmTimingTest.py
@@ -3,48 +3,39 @@ import numpy as np
 import time
 
 import matplotlib.pyplot as plt
-import sys
-sys.path.append('..')
-import rbm_rm
-import rbm_cm
-import utils
-import Preprocess
+import pydeep.rbm
+import pydeep.utils
+import pydeep.utils.utils
 
+mnist_dir = os.path.join(os.environ['PYDEEP_HOME'], 'demo', 'mnist')
+mnist_train_path = os.path.join(mnist_dir, 'train-images-idx3-ubyte.npy')
 
+data = np.load(mnist_train_path).astype('float') / 255
+train = data[:, :]
+valid = data[10000:11000, :]
 
-mnist_dir = os.path.join(os.environ['DATA_HOME'], 'mnist')
-mnist_train_path = os.path.join(mnist_dir, 'MNISTTrainData.npy')
+print ('Train shape %dx%d' % train.shape)
+print ('Valid shape %dx%d' % valid.shape)
 
-data_rm = np.load(mnist_train_path)
-train_rm = data_rm[:, :]
-valid_rm = data_rm[:1000, :]
-
-print ('Train shape %dx%d' % train_rm.shape)
-print ('Valid shape %dx%d' % valid_rm.shape)
-
-nHidden = 500
-viewShape = (25, 20)    # These should multiply to be nHidden
-TP = rbm_rm.RBMTrainParams()
+nHidden = 100
+viewShape = (10, 10)    # These should multiply to be nHidden
+TP = pydeep.rbm.RBMTrainParams()
 TP.maxepoch = 15
 
-rm_learner = rbm_rm.RBM(nHidden, train_rm.shape[1])
-# rm_learner.setVfromSample(train_rm)
+rbm = pydeep.rbm.RBM(nHidden, train.shape[1])
+# rm_learner.setVfromSample(train) <- Hinton says to do this, but I don't find
+#                                     that it works very well
 
 start_time = time.clock()
-rbm_rm.learn(rm_learner, train_rm, valid_rm, TP) 
+pydeep.rbm.learn(rbm, train, valid, TP)
 end_time = time.clock()
 print ('Training took %f minutes' % ((end_time-start_time)/60.))
 
-
-
-Wimg_rm = utils.tile_raster_images(rm_learner.W.transpose(), (28, 28), viewShape)
+Wimg = pydeep.utils.utils.tile_raster_images(rbm.W.transpose(), (28, 28), viewShape)
 
 plt.figure(1)
-plt.subplot(121)
-plt.imshow(Wimg_rm)
+plt.imshow(Wimg)
 plt.set_cmap('gray')
 plt.axis('off')
-plt.subplot(122)
-plt.imshow(Wimg_cm)
-plt.axis('off')
 plt.show()
+
