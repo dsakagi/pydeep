e9e690b3516a9d6249995d05faf3d452f412bb00
Dallin Akagi <dallin@datarobot.com>
2013-10-11 21:46:51 -0400

    One more demo works

--- debug/rbm_gaussTest.py
+++ debug/rbm_gaussTest.py
@@ -1,53 +1,36 @@
 import os
 import sys
-sys.path.append('..')
 import numpy as np
-import rbm_rm
-import rbm_cm
+import pydeep.rbm
 
 import matplotlib.pyplot as plt
-import utils
-import Preprocess
+import pydeep.utils.preprocess
+import pydeep.utils.utils
 
+mnist_dir = os.path.join(os.environ['PYDEEP_HOME'], 'demo', 'mnist')
+mnist_train_path = os.path.join(mnist_dir, 'train-images-idx3-ubyte.npy')
 
-
-mnist_dir = os.path.join(os.environ['DATA_HOME'], 'mnist')
-mnist_train_path = os.path.join(mnist_dir, 'MNISTTrainData.npy')
-
-data_rm = np.load(mnist_train_path)
-[normed, meanv, stdv] = Preprocess.mean_zero_unit_variance(data_rm)
+data =  np.load(mnist_train_path).astype('float')/255
+[normed, meanv, stdv] = pydeep.utils.preprocess.mean_zero_unit_variance(data)
 #Look, I didn't actually use the normalized data because it broke everything
 
-train_rm = data_rm[30000:, :]
-valid_rm = data_rm[:30000, :]
-
-data_cm = data_rm.transpose()
-train_cm = data_cm[:,30000:]
-valid_cm = data_cm[:,:30000]
+train = data[50000:, :]
+valid = data[:30000, :]
 
 nHidden = 100
 ViewDimensions = (10, 10)   # Should multiply to nHidden
-TP = rbm_rm.RBMTrainParams()
+TP = pydeep.rbm.RBMTrainParams()
+TP.eta = pydeep.scalar_schedule.ConstantSchedule(0.001)
 TP.maxepoch = 15
 
-rm_learner = rbm_rm.GV_RBM(nHidden, train_rm.shape[1])
-#rm_learner.setVfromSample(train_rm)         # For some reason, this is the worst idea in the world
-rbm_rm.learn(rm_learner, train_rm, valid_rm, TP) 
+rbm = pydeep.rbm.GV_RBM(nHidden, train.shape[1])
+#rm_learner.setVfromSample(train)         # For some reason, this is the worst idea in the world
+pydeep.rbm.learn(rbm, train, valid, TP)
 
-cm_learner = rbm_cm.GV_RBM(nHidden, train_cm.shape[0])
-#cm_learner.setVfromSample(train_cm)         # For some reason, this is the worst idea in the world
-rbm_cm.learn(cm_learner, train_cm, valid_cm, TP)
-
-
-Wimg_rm = utils.tile_raster_images(rm_learner.W.transpose(), (28, 28), ViewDimensions)
-Wimg_cm = utils.tile_raster_images(cm_learner.W, (28, 28), ViewDimensions)
+Wimg = pydeep.utils.utils.tile_raster_images(rbm.W.transpose(), (28, 28), ViewDimensions)
 
 plt.figure(1)
-plt.subplot(121)
-plt.imshow(Wimg_rm)
+plt.imshow(Wimg)
 plt.set_cmap('gray')
-plt.axis('off')
-plt.subplot(122)
-plt.imshow(Wimg_cm)
 plt.axis('off')
 plt.show()
